43eeae80535bfe9561bdc0b71109b9dd56dd1146
SIFOUYT3412 <85214811+SIFOUYT3412@users.noreply.github.com>
2021-06-19 20:38:05 +0100

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     },
 
     discord: {
-        token: 'TOKEN',
-        prefix: 'PREFIX',
-        activity: 'ACTIVITY',
+        token: 'ODU1NjMyMzg0OTI5MTY5NDA5.YM1TwQ.5fgb6oG_isGQVtg_1R5PAAgM9co',
+        prefix: 'é',
+        activity: '2gelp',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
